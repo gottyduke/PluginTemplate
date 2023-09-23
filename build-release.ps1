d1dbf1ef0dd97876fd0598133ddbf91ce8841192
vcpkg-action <gottyduke@hotmail.com>
2023-09-23 16:21:12 +0800

    feat: bring script changes from upstream

--- build-release.ps1
+++
@@ -1,3 +0,0 @@
-Remove-Item $PSScriptRoot/build -Recurse -Force -ErrorAction:SilentlyContinue -Confirm:$False | Out-Null
-& cmake -B $PSScriptRoot/build -S $PSScriptRoot/PluginTemplate --preset=REL -DPLUGIN_MODE:BOOL=TRUE
-& cmake --build $PSScriptRoot/build --config Release
